41807c6327b5e6dbe0adc4caababd05caa5c72f1
许飞 <feixu@zhejianglab.com>
2024-11-06 11:06:30 +0800

    first

--- app/controllers/course1_controller.py
+++ app/controllers/course1_controller.py
@@ -4,8 +4,14 @@ from app.util.clickhouse import ck_client
 from collections import namedtuple
 
 BlsElectInfo = namedtuple('BlsElectInfo', ['id', 'elect_height', 'shard_id', 'member_idx', 'contribution_map', 'local_sk', 'common_pk'])
+BlsBlockInfo = namedtuple('BlsBlockInfo', ['id', 'elect_height', 'view', 'shard_id', 'leader_idx', 'partial_sign_map', 'reconstructed_sign', 'common_pk'])
+
 
 def get_bls_elect_info_list(limit: int = 10, offset: int = 0):
     elect_infos = ck_client.execute('select * from bls_elect_info order by elect_height desc limit 10 offset 0;')
     return [BlsElectInfo(*row) for row in elect_infos]
 
+
+def get_bls_block_info_list(limit: int = 10, offset: int = 0):
+    block_infos = ck_client.execute('select * from bls_block_info order by view desc limit 10 offset 0;')
+    return [BlsBlockInfo(*row) for row in block_infos]
--- app/http/handlers/course.py
+++ app/http/handlers/course.py
@@ -1,15 +1,16 @@
 # coding: utf-8
 
 from flask import Blueprint, jsonify, render_template
-from app.controllers.course1_controller import get_bls_elect_info_list
+from app.controllers.course1_controller import get_bls_elect_info_list, get_bls_block_info_list
 
 course_router = Blueprint('course_router', __name__)
 
-@course_router.route('/course1')
-def course1():
-    elect_infos = get_bls_elect_info_list();
-    return render_template('course1/index.html', elect_infos=elect_infos)
+@course_router.route('/course1/elect_info_list')
+def elect_info_list():
+    elect_infos = get_bls_elect_info_list()
+    return render_template('course1/elect_info_list.html', elect_infos=elect_infos)
 
-@course_router.route('/course1/bls_elect_info_list')
-def bls_elect_info_list():
-    return jsonify({'data': get_bls_elect_info_list()})
+@course_router.route('/course1/block_info_list')
+def block_info_list():
+    block_infos = get_bls_block_info_list()
+    return render_template('course1/block_info_list.html', block_infos=block_infos)
